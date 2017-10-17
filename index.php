bf9b5e10b2bb36aaaaa14e3164881b44d0d06e3b
Victor Garcez <victor_garcez@msn.com>
2017-10-16 23:07:23 -0200

    finished api

--- index.php
+++ index.php
@@ -5,18 +5,18 @@ use projeto\entities\names;
 use projeto\controller\namesController;
 
 $app = new \Slim\Slim();
-
+$app->response()->header('Content-Type', 'application/json;charset=utf-8');
 $namesCtrl = new namesController();
 
 
 $app->get ( '/' , function() use ($namesCtrl){
-    echo json_encode($namesCtrl->getALL()); 
+    echo(json_encode($namesCtrl->getALL(), JSON_PRETTY_PRINT)); 
 });
 
 $app->post('/', function() use ($namesCtrl){
     $app = \Slim\Slim::getInstance();
     $json = json_decode($app->request()->getBody());
-    echo json_encode($usuarioCtrl->insert($json));
+    echo json_encode($namesCtrl->insert($json));
 });
 
 $app->post('/', function() use ($namesCtrl){
